70c0da8bb917cc42684b182714c5405e9c9efe79
miyagaw61 <miyagaw61@gmail.com>
2017-09-16 02:04:39 +0900

    [bugfix]setup.py :

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="enermemo",
     version="0.0.1",
     packages=find_packages(),
-    description="memo script"
+    description="memo script",
     author="Taisei Miyagawa <Twitter: @miyagaw61>",
     author_email="miyagaw61@gmail.com",
     install_requires=['enert'],
