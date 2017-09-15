e620a1ac695c59955b32eacd1db018fa802fcb0a
miyagaw61 <miyagaw61@gmail.com>
2017-09-16 02:23:42 +0900

    [bugfix]setup.py :

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Taisei Miyagawa <Twitter: @miyagaw61>",
     author_email="miyagaw61@gmail.com",
     install_requires=['enert'],
-    dependency_links=['git+ssh://git@github.com/miyagaw61/enert.git#egg=enert'],
+    dependency_links=['git+https://github.com/miyagaw61/enert.git#egg=enert'],
     entry_points = {'console_scripts': ['enermemo=enermemo.py']},
     url="https://github.com/miyagaw61/enermemo.git",
     license="GPL-3.0"
